85dd7a242e16e5bc33a904c4855c03be01508ae3
jtejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-04-29 16:08:05 -0600

    se agregan validaciones para nombre y descripcion de tareas

---
+++ src/shared/validators/validateDescripcion.jsx
@@ -0,0 +1,6 @@
+export const validateDescripcion = (descripcion) => {
+
+    return descripcion.length >= 10 && descripcionlength <= 250
+}
+
+export const validateDescripcionMessagge = 'La descriptión debe de tener entre 10 y 250 caracteres'
