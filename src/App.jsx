e5fd9dcd4711e40ad43ea709df18c1f983c7856f
osanchez <osanchez-2019490@kinal.edu.gt>
2024-05-01 12:50:05 -0600

    Se quita import nada que ver

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LoadingSpinner } from './components/general/LoadingSpinner'
 
 function App() {
   const [count, setCount] = useState(0)
